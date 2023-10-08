5e43630755d21610243259e6697c28fbcdbe0b99
LAPTOP-FG35BCEI\FM <2412760825@qq.com>
2023-10-08 13:05:17 +0800

    1、针对项目调整JsonMgr 2、引入常量数据类来存放一些常量数据

---
+++ Assets/Game/Scripts/Application/Misc/Consts.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 常量数据类
+/// </summary>
+public static class Consts
+{
+    // 关卡配置文件路径
+    public static readonly string LevelDir = "";
+    // 地图资源路径
+    public static readonly string MapDir = "";
+}
--- Assets/Game/Scripts/FrameWork/Json/JsonMgr.cs
+++ Assets/Game/Scripts/FrameWork/Json/JsonMgr.cs
@@ -12,22 +12,21 @@ public enum JsonType
 }
 
 // Json管理类
-public class JsonMgr
+public class JsonMgr : BaseManager<JsonMgr>
 {
-    private static JsonMgr instance = new JsonMgr();
-    public static JsonMgr Instance => instance;
-
-    private JsonMgr() { }
-
     /// <summary>
     /// 存储数据
     /// </summary>
     /// <param name="data"> 数据 </param>
     /// <param name="fileName"> 文件名 </param>
+    /// <param name="filePath"> 文件路径 </param>
     /// <param name="type"> 选用的方法，默认采用LitJson</param>
-    public void SaveData(object data, string fileName, JsonType type = JsonType.LitJson)
+    public void SaveData(object data, string fileName, string filePath = null, JsonType type = JsonType.LitJson)
     {
-        string path = Application.persistentDataPath + "/" + fileName + ".json";
+        if (filePath == null)
+            filePath = Application.persistentDataPath;
+
+        string path = filePath + "/" + fileName + ".json";
 
         string jsonStr = "";
         switch (type)
@@ -47,16 +46,20 @@ public class JsonMgr
     /// 读取数据
     /// </summary>
     /// <param name="fileName"> 文件名 </param>
+    /// <param name="filePath"> 文件路径 </param>
     /// <param name="type"> 选用的方法，默认为LitJson</param>
     /// <typeparam name="T"> 数据类型 </typeparam>
     /// <returns></returns>
-    public T LoadData<T>(string fileName, JsonType type = JsonType.LitJson) where T : new()
+    public T LoadData<T>(string fileName, string filePath = null, JsonType type = JsonType.LitJson) where T : new()
     {
+        if (filePath == null)
+            filePath = Application.persistentDataPath;
+
         // 首先从默认文件夹中（StreamingAsset）读取
         string path = Application.streamingAssetsPath + "/" + fileName + ".json";
         // 如果没有 则从读写文件夹中读取
         if (!File.Exists(path))
-            path = Application.persistentDataPath + "/" + fileName + ".json";
+            path = filePath + "/" + fileName + ".json";
         // 如果还没有 则返回默认值
         if (!File.Exists(path))
             return new T();
